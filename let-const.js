f6b367ffbc94b2650ea8ca06a0cca6135055fa97
jobaydulcse <jobaydul.cse@gmail.com>
2025-08-30 03:29:12 +0600

    ghumamu rait 4 ta bajche

--- let-const.js
+++ let-const.js
@@ -6,3 +6,15 @@ console.log(countryName);
 let price = 120;
 price = 200;
 console.log(price);
+
+//
+const charge = 50;
+// charge = 40;
+
+const dress = ["shart", "pant", "jaket"];
+dress.push("Jaingga");
+console.log(dress);
+
+const student = { name: "Jaglul", marks: 50 };
+student.marks = 60;
+console.log(student);
