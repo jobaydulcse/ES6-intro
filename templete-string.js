e1a8e0a2de52be9228aab2261893c6045653afbd
jobaydulcse <jobaydul.cse@gmail.com>
2025-08-30 12:11:26 +0600

    ektu ghumamu

---
+++ templete-string.js
@@ -0,0 +1,17 @@
+const poem = `Am pata Jora Jora
+marbo cabuk chorbo ghora 
+doooo do doooo doooooooo`;
+// console.log(poem);
+
+function sum(num1, num2) {
+  const result = num1 + num2;
+  const output = `sum of ${num1 * 5} and ${num2} is equal to ${result}`;
+  //   console.log(output);
+}
+sum(100, 100);
+
+const price = 100;
+const discountedPrice = `Discounted price of ${price} after 20% discount ${
+  price * 0.8
+}`;
+console.log(discountedPrice);
